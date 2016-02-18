fd8ca19fefc09686c4f12a320bed3e0382046f65
Jason Warta <jdwarta@alaska.ed>
2016-02-17 17:12:23 -0900

    retracked .meteor and .gagarin folders

--- battleship/tests/gagarin/dummy.js
+++ battleship/tests/gagarin/dummy.js
@@ -1,15 +1,27 @@
 // tests/gagarin/dummy.js
-describe('A dummy test suite', function () {
-	var server = meteor();
+// describe('A dummy test suite', function () {
+// 	var server = meteor();
 
-  it('should do nothing', function () {});
+//   // it('should do nothing', function () {});
 
-  it('execute should work', function () {
-    // return a promise
-    return server.execute(function () {
-      expect(Meteor.release).not.to.be.empty;
-    });
-  });
+//   // it('execute should work', function () {
+//   //   // return a promise
+//   //   return server.execute(function () {
+//   //     expect(Meteor.release).not.to.be.empty;
+//   //   });
+//   // });
 
+	
   
+// });
+
+describe("Cell generation test", function(){
+	var server = meteor();
+	var cleint = browser(server);
+
+	it("Cells should created", function() {
+		var output = client.execute( 'initGridMeteor' );
+
+			expect(output).to.equal("success");
+	});
 });
